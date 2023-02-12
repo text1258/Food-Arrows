f364c84fd99f45a54d872b931010abcc11f2958e
Text <myhewacc.09209@gmail.com>
2023-02-12 10:44:01 +0300

    Build

--- Assets/Scripts/ShootClub/Missiles/CannonMissile.cs
+++ Assets/Scripts/ShootClub/Missiles/CannonMissile.cs
@@ -1,1 +1,25 @@
-﻿public class CannonMissile : Missile {}
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CannonMissile : Missile
+{
+    [SerializeField] private float timeBeforeDestroy;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Target collisionTarget = collision.gameObject.GetComponent<Target>();
+        if (collisionTarget != null)
+        {
+            collisionTarget.notHitByMissile = false;
+            collision.gameObject.GetComponent<Collider>().enabled = false;
+            collision.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            foreach (Transform child in collision.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+            collisionTarget.targetSpawner.SpawnTarget();
+            Destroy(collision.gameObject, collision.gameObject.GetComponent<Target>().TimeBeforeDestroy);
+        }
+        Destroy(this.gameObject, timeBeforeDestroy);
+    }
+}
